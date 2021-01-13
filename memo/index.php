103125f5b3ca56ee516f0f91eb30f0d75b4cf92b
georgia <georgia6954@gmail.com>
2021-01-13 22:48:04 +0900

    メモ表示　修正

--- memo/index.php
+++ memo/index.php
@@ -53,45 +53,46 @@ if (isset($_SESSION['select_memo'])) {
                 メモリスト
             </div>
             <div class="left-memo-list list-group-flush p-0">
-                <a href="#" class="list-group-item list-group-item-action">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">メモタイトル1</h5>
-                        <small>2020/08/01 09:00</small>
+                <?php if(empty($memos)): ?>
+                    <div class="pl-3 pt-3 h5 text-info text-center">
+                        <i class="far fa-surprise"></i>メモがありません。
                     </div>
-                    <p class="mb-1">
-                        メモ詳細1
-                    </p>
-                </a>
-                <a href="#" class="list-group-item list-group-item-action">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">メモタイトル2</h5>
-                        <small>2020/08/01 09:00</small>
-                    </div>
-                    <p class="mb-1">
-                        メモ詳細2
-                    </p>
-                </a>
-                <a href="#" class="list-group-item list-group-item-action">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">メモタイトル3</h5>
-                        <small>2020/08/01 09:00</small>
-                    </div>
-                    <p class="mb-1">
-                        メモ詳細3
-                    </p>
-                </a>
+                <?php endif; ?>
+                <?php foreach($memos as $memo): ?>
+                    <a href="./action/select.php?id=<?php echo $memo['id']; ?>" class="list-group-item list-group-item-action <?php echo $edit_id == $memo['id'] ? 'active' : ''; ?>">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1"><?php echo $memo["title"] ?></h5>
+                            <small><?php echo date('Y/m/d H:i', strtotime($memo['updated_at'])); ?></small>
+                        </div>
+                        <p class="mb-1">
+                            <?php
+                            if (mb_strlen($memo['content']) <= 100) {
+                                echo $memo['content'];
+                            } else {
+                                echo mb_substr($memo['content'], 0, 100) . "...";
+                            }
+                            ?>
+                        </p>
+                    </a>
+                <?php endforeach; ?>
             </div>
         </div>
         <div class="col-9 h-100">
-            <form class="w-100 h-100" method="post">
-                <input type="hidden" name="edit_id" value="" />
-                <div id="memo-menu">
-                    <button type="submit" class="btn btn-danger" formaction=""><i class="fas fa-trash-alt"></i></button>
-                    <button type="submit" class="btn btn-success" formaction=""><i class="fas fa-save"></i></button>
+            <?php if(isset($_SESSION['select_memo'])): ?>
+                <form class="w-100 h-100" method="post">
+                    <input type="hidden" name="edit_id" value="<?php echo $edit_id; ?>" />
+                    <div id="memo-menu">
+                        <button type="submit" class="btn btn-danger" formaction="./action/delete.php"><i class="fas fa-trash-alt"></i></button>
+                        <button type="submit" class="btn btn-success" formaction="./action/update.php"><i class="fas fa-save"></i></button>
+                    </div>
+                    <input type="text" id="memo-title" name="edit_title" placeholder="タイトルを入力する..." value="<?php echo $edit_title; ?>" />
+                    <textarea id="memo-content" name="edit_content" placeholder="内容を入力する..."><?php echo $edit_content; ?></textarea>
+                </form>
+            <?php else: ?>
+                <div class="mt-3 alert alert-info">
+                    <i class="fas fa-info-circle"></i>メモを新規作成するか選択してください。
                 </div>
-                <input type="text" id="memo-title" name="edit_title" placeholder="タイトルを入力する..." value="" />
-                <textarea id="memo-content" name="edit_content" placeholder="内容を入力する..."></textarea>
-            </form>
+            <?php endif; ?>
         </div>
     </div>
 </div>
